71bca1ecc64e00efeafb551a3da0e7e5c2aae590
axel <axel.bouillart@gmail.com>
2017-01-24 14:18:32 +0100

    push

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
 
 
-echo "<h1>".count($dico)."</h1>";
+echo "<h1>".count($dico)."</h1>"; 
 
 
 
