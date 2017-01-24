470d66f523ea424fb00224ab530360d25ba220ba
axel <axel.bouillart@gmail.com>
2017-01-24 14:57:09 +0100

    step 1 exo 2

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
-
-
 <?php 
+
 
 $string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 $dico = explode("\n", $string);
+$stringCount15 =array();
 
 
-echo "<h1>".count($dico)."</h1>"; 
+
+foreach ($dico as $string) {
+	if (strlen($string) == 15) {
+		array_push($stringCount15, $string);
+	}
+}
+
+echo "<h1>".count($stringCount15)."</h1>"; 
 
 
 
